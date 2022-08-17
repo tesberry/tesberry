04bfb6391f027d4ae0a1d12f2100cfb67d5a41d0
Paul Tiedtke <PaulTiedtke@web.de>
2022-08-17 16:58:41 +0200

    fix: some bugs in savvycan-mqtt script

--- docker/savvycan-mqtt/savvycan-mqtt.py
+++ docker/savvycan-mqtt/savvycan-mqtt.py
@@ -13,11 +13,11 @@ import ssl
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-	    print("Connected with result code "+str(rc))
-	
-	    # Subscribing in on_connect() means that if we lose the connection and
-	    # reconnect then subscriptions will be renewed.
-	    client.subscribe(topic + "/+", qos=0)
+	print("Connected with result code "+str(rc))
+
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	client.subscribe(arg_results.topic + "/+", qos=0)
 	
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
